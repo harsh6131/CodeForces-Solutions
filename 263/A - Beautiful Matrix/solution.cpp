30d9c29a5c4fa031e98ae29fe8f97e320e582a0f
Harshvardhan Singh Jaisawat <harshvardhan.24bce11064@vitbhopal.ac.in>
2026-08-20 12:41:39 +0530

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int row, col;
+ 
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+ 
+            int x;
+            cin >> x;
+ 
+            if (x == 1) {
+                row = i;
+                col = j;
+            }
+        }
+    }
+ 
+    cout << abs(row - 3) + abs(col - 3);
+ 
+    return 0;
+}
