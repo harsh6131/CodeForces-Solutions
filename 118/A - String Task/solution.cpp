3f10d9d52c2934aa40a002860057535d1b57ba53
Harshvardhan Singh Jaisawat <harshvardhan.24bce11064@vitbhopal.ac.in>
2026-08-20 12:30:38 +0530

    Add String Task [A] from Codeforces

---
+++ 118/A - String Task/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+ 
+    for (char c : s) {
+ 
+        c = tolower(c);
+ 
+        if (c == 'a' || c == 'o' || c == 'y' ||
+            c == 'e' || c == 'u' || c == 'i') {
+            continue;
+        }
+ 
+        cout << "." << c;
+    }
+ 
+    return 0;
+}
