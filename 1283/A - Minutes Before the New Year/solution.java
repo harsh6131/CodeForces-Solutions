f671d55782acea535771641cd24a1ab491e3afdb
Harshvardhan Singh <harshvardhan.24bce11064@vitbhopal.ac.in>
2026-09-16 23:01:29 +0530

    Add Minutes Before the New Year [A] from Codeforces

---
+++ 1283/A - Minutes Before the New Year/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int h = sc.nextInt();
+            int m = sc.nextInt();
+ 
+            int answer = 1440 - (h * 60 + m);
+ 
+            System.out.println(answer);
+        }
+    }
+}
