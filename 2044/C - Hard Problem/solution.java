b548e4b82caa9e76beaecf2f4a2bb72cd9aba96a
Harshvardhan Singh <harshvardhan.24bce11064@vitbhopal.ac.in>
2026-09-24 23:42:15 +0530

    Add Hard Problem [C] from Codeforces

---
+++ 2044/C - Hard Problem/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            long m = sc.nextLong();
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+ 
+            long row1 = Math.min(a, m);
+            long row2 = Math.min(b, m);
+ 
+            long empty = (m - row1) + (m - row2);
+ 
+            long flexible = Math.min(c, empty);
+ 
+            long answer = row1 + row2 + flexible;
+ 
+            System.out.println(answer);
+        }
+    }
+}
